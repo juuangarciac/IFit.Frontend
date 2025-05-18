7b5604f934dc222fe33e5ae1d162e47bd049093c
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2025-05-18 19:52:22 +0200

    Primer commit. Creación del proyecto .NET MAUI

---
+++ IFit/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace IFit
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ IFit/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace IFit
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
