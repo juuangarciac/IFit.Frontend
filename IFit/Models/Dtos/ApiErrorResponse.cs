086dec62ddf14247a8affa6717711329404b7e3e
Juan Garcia Candon <113298385+juuangarciac@users.noreply.github.com>
2026-01-25 19:48:49 +0100

    FEAT. ApiErrorResponse. DTO para gestionar los mensajes de error de la API.

---
+++ IFit/Models/Dtos/ApiErrorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+public class ApiErrorResponse
+{
+    [JsonPropertyName("timestamp")]
+    public string? Timestamp { get; set; }
+
+    [JsonPropertyName("path")]
+    public string? Path { get; set; }
+
+    [JsonPropertyName("status")]
+    public int Status { get; set; }
+
+    [JsonPropertyName("error")]
+    public string? Error { get; set; }
+
+    [JsonPropertyName("requestId")]
+    public string? RequestId { get; set; }
+}
